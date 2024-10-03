5b243eace517fb6a650ccaad8293794cac9d8920
MubinQazi123 <135802820+MubinQazi123@users.noreply.github.com>
2024-10-03 20:17:41 +0000

    second

---
+++ javascript1/02Variables.js
@@ -0,0 +1,14 @@
+const accountId = 12344
+let accountEmail = "mubin@gmsil.com"
+var accountPassword = "897867"
+accountCity = "Solapur"
+
+// accountId = 2 not allowed beacuse account id is constant
+
+ accountEmail = "mk@gmsil.com"
+ accountPassword = "000000"
+accountCity = "pune"
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
